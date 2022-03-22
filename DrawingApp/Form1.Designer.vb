493b4f92b0a48f65e4311ff3d06f4b50c6aa6139
Amil <agyir598@learner.dvusd.org>
2022-03-22 13:23:37 -0700

    Test

--- DrawingApp/Form1.Designer.vb
+++ DrawingApp/Form1.Designer.vb
@@ -56,7 +56,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(687, 191)
+        Me.Button1.Location = New System.Drawing.Point(730, 191)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(26, 25)
         Me.Button1.TabIndex = 1
